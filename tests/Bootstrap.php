b9ec6b962dd66d6b805f932b40f39d1fd1daacde
Raymond Kolbe <rkolbe@gmail.com>
2016-10-29 21:36:02 -0400

    Travis experiment #7

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,8 +3,8 @@
 use DOMPDFModuleTest\Framework\TestCase;
 use Zend\ServiceManager\ServiceManager;
 use Zend\Mvc\Service\ServiceManagerConfig;
-use Zend\Loader\StandardAutoloader;
 
+ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
 
 if (is_readable(__DIR__ . '/TestConfiguration.php')) {
@@ -13,17 +13,7 @@ if (is_readable(__DIR__ . '/TestConfiguration.php')) {
     $configuration = include_once __DIR__ . '/TestConfiguration.php.dist';
 }
 
-// Assumes PHP Composer autoloader w/compiled classmaps, etc.
 require_once __DIR__ . '/../vendor/autoload.php';
-
-// This namespace is not in classmap.
-$loader = new StandardAutoloader(
-                array(
-                    StandardAutoloader::LOAD_NS => array(
-                        'DOMPDFModuleTest' => __DIR__ . '/DOMPDFModuleTest'
-                    ),
-        ));
-$loader->register();
 
 $serviceManager = new ServiceManager(new ServiceManagerConfig($configuration['service_manager']));
 $serviceManager->setService('ApplicationConfig', $configuration);

c267c07f6bfd2c9eefdf40f181625c9e75c584cd
Raymond Kolbe <raykolbe@users.noreply.github.com>
2016-10-29 21:48:05 -0400

    Fix Travis CI build (#42)
    * Corrected dependency definitions.
    * Simplified and fixed Travis CI build.

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -3,24 +3,9 @@
 use DOMPDFModuleTest\Framework\TestCase;
 use Zend\ServiceManager\ServiceManager;
 use Zend\Mvc\Service\ServiceManagerConfig;
-use Zend\Loader\StandardAutoloader;
 
+ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
-
-chdir(__DIR__);
-
-$previousDir = '.';
-while (!file_exists('config/application.config.php')) {
-    $dir = dirname(getcwd());
-    if ($previousDir === $dir) {
-        throw new RuntimeException(
-                'Unable to locate "config/application.config.php": ' .
-                'is DOMPDFModule in a subdir of your application skeleton?'
-        );
-    }
-    $previousDir = $dir;
-    chdir($dir);
-}
 
 if (is_readable(__DIR__ . '/TestConfiguration.php')) {
     $configuration = include_once __DIR__ . '/TestConfiguration.php';
@@ -28,17 +13,7 @@ if (is_readable(__DIR__ . '/TestConfiguration.php')) {
     $configuration = include_once __DIR__ . '/TestConfiguration.php.dist';
 }
 
-// Assumes PHP Composer autoloader w/compiled classmaps, etc.
-require_once 'vendor/autoload.php';
-
-// This namespace is not in classmap.
-$loader = new StandardAutoloader(
-                array(
-                    StandardAutoloader::LOAD_NS => array(
-                        'DOMPDFModuleTest' => __DIR__ . '/DOMPDFModuleTest'
-                    ),
-        ));
-$loader->register();
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $serviceManager = new ServiceManager(new ServiceManagerConfig($configuration['service_manager']));
 $serviceManager->setService('ApplicationConfig', $configuration);
